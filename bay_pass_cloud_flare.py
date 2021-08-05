2ba3459552ba25f4900b6e4c97e6bf1788c7eb06
Mohammad <88484934+trabit373@users.noreply.github.com>
2021-08-05 22:30:27 +0430

    Update bay_pass_cloud_flare.py

--- bay_pass_cloud_flare.py
+++ bay_pass_cloud_flare.py
@@ -11,16 +11,16 @@ def _all_():
     text = Fore.LIGHTWHITE_EX + "  [!] " +  Fore.CYAN + "Enter Your Domain Target !\n"
     count = 0
     while True:
-    try:
-        text_me = text[int(count)]
-        sys.stdout.write(f"{str(text_me)}"),sys.stdout.flush()
-        count += 1
-        time.sleep(0.08)
-    except:
-        print("")
-        break
+        try:
+            text_me = text[int(count)]
+            sys.stdout.write(f"{str(text_me)}"),sys.stdout.flush()
+            count += 1
+            time.sleep(0.08)
+        except:
+            print("")
+            break
     target =  input(Fore.RED+"  ┌─["+Fore.GREEN+"H_NWWB"+Fore.BLUE+"~"+Fore.WHITE+"@Information-Data-For-IP"+Fore.RED+"/"+Fore.CYAN+"IG"+Fore.RED+"/"+Fore.LIGHTYELLOW_EX+"bay-pass-cloud-flare"+Fore.RED+"""]
-    └──╼ """+Fore.WHITE+"卐 ")
+  └──╼ """+Fore.WHITE+"卐 ")
     if target == "" or None:
         time.sleep(1)
         print(Fore.YELLOW + "["+ Fore.RED + "!" + Fore.YELLOW + "]"+ Fore.RED + " Error" + Fore.YELLOW + " >>> " + Fore.RED + "Your Target Ip None !!!")
